5138236027047bae099b34b3f61feae6638a9679
Emelí Pasini <emelipasini@outlook.com>
2024-01-03 23:04:36 +0100

    refactor password validator to return an array of errors

--- katas/src/password-validator.ts
+++ katas/src/password-validator.ts
@@ -1,10 +1,17 @@
 export default function passwordValidator(password: string) {
-    if (password.length < 8) return "Error: The password must have at least 8 characters";
-    if (!hasUppercaseLetter(password)) return "Error: The password must have at least 1 uppercase letter";
-    if (!hasLowercaseLetter(password)) return "Error: The password must have at least 1 lowercase letter";
-    if (!hasNumber(password)) return "Error: The password must have at least 1 number";
-    if (!hasSpecialCharacter(password)) return "Error: The password must have at least 1 special character";
-    return true;
+    const errors: string[] = [];
+
+    if (!hasAtLeast8Characters(password)) saveError("8 characters", errors);
+    if (!hasUppercaseLetter(password)) saveError("1 uppercase letter", errors);
+    if (!hasLowercaseLetter(password)) saveError("1 lowercase letter", errors);
+    if (!hasNumber(password)) saveError("1 number", errors);
+    if (!hasSpecialCharacter(password)) saveError("1 special character", errors);
+
+    return errors.length === 0 ? true : errors;
+}
+
+function hasAtLeast8Characters(password: string) {
+    return password.length >= 8;
 }
 
 function hasUppercaseLetter(password: string) {
@@ -20,5 +27,9 @@ function hasNumber(password: string) {
 }
 
 function hasSpecialCharacter(password: string) {
-    return /.*[!-\/].*/.test(password);
+    return /.*[!-\/:-@[-`{-~].*/.test(password);
+}
+
+function saveError(condition: string, errors: string[]) {
+    errors.push(`The password must have at least ${condition}`);
 }
